5b087e3786216d1e872885af88968098ed86d634
CaloPando <49101927+CaloPando@users.noreply.github.com>
2020-02-16 16:11:43 +0100

    Update Neal_1D_regression.py

--- Neal_1D_regression.py
+++ Neal_1D_regression.py
@@ -11,6 +11,9 @@ as mean of the priors for the Gaussians in the next sampling sessions, a procedu
 but was an attempt to have the weights update themselves gradually 
 '''
 
+num_iterations=150
+num_warmup=100
+num_train=60
 D = 100
 I = 1
 O = 1
@@ -35,22 +38,27 @@ for epoch in range(num_epochs):
     #This line is necessary because there is apparently a flaw with how python multiprocessing was implemented in Windows
     #Without, it is impossible to run multiple cores
     if __name__ == '__main__':
-        context = np.random.choice(list(range(D)), 30)
+        context = np.random.choice(list(range(D)), num_train)
         context.sort()
-        model.train(x=x[:,context],y=y[:,context],iter=50,warmup=30,max_treedepth=10, thin=2,chains=4,cores=4)
+        model.train(x=x[:,context],y=y[:,context],iter=num_iterations, warmup=num_warmup,max_treedepth=10, thin=2,chains=4,cores=4)
         #model.save_parameters('network_5000_20')
         print(model.fit)
         mu, sigma = model.predict(x_pred=x)
+        
+        #This computes the log likelihood
         prob = 0
         for d in range(D):
-            prob += norm(mu[0, d], sigma[0, d]).pdf(y[0, d])
+            prob += np.log(norm(mu[0, d], sigma[0, d]).pdf(y[0, d])) 
         prob=prob/D
-        print("Average probability of real value:%f" % prob)
-        output_variance = np.mean(model.output_variance)
-        print(output_variance)
+        print("Log likelihood:%f" % prob)
+        
+        #This is an estimate of the std of the random noise of the original function, we notice, that while good at capturing
+        #epistemic uncertainty, it has a regularizing effect, so tends to disregard high levels of noise and fit the real function
+        print('estimated standard deviation ' + str(np.mean(sigma[0,:])))
+        
         '''
         Notice the quantiles of the normal, no Bonferroni correction was applied, Deepmind's code of Neural Processes
-        disregarded them completely and just added the variance 
+        disregards them completely and just adds the variance 
         '''
         plt.fill_between(x[0, :], mu[0,:] - 1.96 * sigma[0,:], mu[0,:] + 1.96 * sigma[0,:], facecolor=(0, 1, 1))
         plt.plot(x[0, context], y[0, context], 'ro')
